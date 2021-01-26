be0fbd943a1a00dfa2a5392a920ea3bd45ad9d43
JoaquinManchenoP <manchenojj@g.cofc.edu>
2021-01-26 17:18:51 -0500

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import { client } from "./client";
 
 function App() {
+  const [entries, setEntries] = useState({
+    bands: [],
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <header>
+          <div className="wrapper">
+            <span>React and contentful</span>
+          </div>
+        </header>
+        <main>
+          <div className="main_content"></div>
+        </main>
+      </div>
     </div>
   );
 }
